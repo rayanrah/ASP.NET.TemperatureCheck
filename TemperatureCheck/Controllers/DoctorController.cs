245025926e88d8852657f4db6064eeb345bc4186
ryan.Rah <re.rahdari@gmail.com>
2024-06-06 16:25:42 +0200

    Add project files.

---
+++ TemperatureCheck/Controllers/DoctorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TemperatureCheck.Models;
+
+namespace TemperatureCheck.Controllers
+{
+    public class DoctorController : Controller
+    {
+        [HttpPost]
+        [Route("FeverCheck")]
+        public IActionResult CheckTemperature(TemperatureModelMV model)
+        {
+            if (!model.Temperature.HasValue)
+            {
+                TempData["ErrorMessage"] = "Please enter your temperature.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            var diagnosis = model.GetDiagnosis();
+            return View("TemperatureDiagnose", diagnosis);
+        }
+
+        [HttpPost]
+        public IActionResult TemperatureDiagnose()
+        {
+            return View();
+        }
+    }
+}
---
+++ TemperatureCheck/Models/TemperatureModelMV.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TemperatureCheck.Models
+{
+    public class TemperatureModelMV
+    {
+        [Display(Name = "Temperature")]
+        public float? Temperature { get; set; }
+        public string GetDiagnosis()
+        {
+
+            if (Temperature.HasValue)
+            {
+                float temperature = Temperature.Value;
+                if (temperature >= 35 && temperature < 37.5)
+                {
+                    return "You have a Normal temperature";
+                }
+                if (temperature >= 37.5 && temperature < 39)
+                {
+                    return "You have a Fever";
+                }
+                if (temperature < 35)
+                {
+                    return "Your temperature is very low";
+                }
+                return "You have a High fever and need to check with the doctor immediately";
+            }
+            return null;
+        }
+    }
+}
